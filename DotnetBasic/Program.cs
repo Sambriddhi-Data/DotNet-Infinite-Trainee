a3facdb00c4db53d154ddf582648d1cb16e189a4
Sambriddhi Shrestha <samshrestha@infinite.com>
2026-08-25 21:38:26 +0545

    Day 1

---
+++ DotnetBasic/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+
+string description = "This is a sample description.";
+int priority = 1;
+string dueDate = "2026-8-24";
+string title = "Dotnet Class";
+bool isComplete = false;
+decimal cost = 200.45m;
+DateTime dueDate2 = DateTime.Now;
+
+Console.WriteLine("The title is: " + title);    
+Console.WriteLine(description);
+Console.WriteLine("The priority level: "+ priority);
+Console.WriteLine("This was written on: " + dueDate);
+Console.WriteLine("The due date is: " + dueDate2);
+Console.WriteLine("The class completed? " + isComplete);
+Console.WriteLine("Cost for the course: " + cost);
